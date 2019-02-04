1dc032d77cb3ff38c96c0e77f52f33e8c65213b9
Jing Cao <jingcao.yale@gmail.com>
2019-02-04 17:18:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
 # your code goes here
+
+
+def remove_non_strings(array)
+end
+
+def count_elements(array)
+end
+
+def merge_data(keys, data)
+end
+
+def find_cool(array)
+end
+
+def organize_schools(schools)
+end
